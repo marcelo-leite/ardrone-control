96e4feb7bfdfcaf308c5a88cbca6652cfb07d18b
marc-leite <leite_marcelo@outlook.com>
2021-07-05 13:21:36 -0300

    rf-control class edit

--- src/rf-control.cpp
+++ src/rf-control.cpp
@@ -78,8 +78,6 @@ class RFControl{
                 }
                 }
 
-                
-
                 command.size = *((uint8_t*)&size[0]);
                 // memcpy(&command.size, &size[0], sizeof(command.size));
                 Serial.println(command.size);
@@ -111,34 +109,34 @@ class RFControl{
             float v[4];
             int i = 0;
             for(i = 0; i < 4; i++){
-            type.concat(cmd[i]);
+                type.concat(cmd[i]);
             }
 
 
             if(type == "TAKE"){
-            ardrone.takeoff();
-            Serial.println("Decolando");
+                ardrone.takeoff();
+                Serial.println("Decolando");
 
             }else if(type == "LAND"){
-            ardrone.land();
-            Serial.println("Aterrissando");
+                ardrone.land();
+                Serial.println("Aterrissando");
 
             }else if(type == "PCMD"){
-            char vbuffer[16];
-            int p;
-            for(i = 0; i < 16; i++){
-                
-                vbuffer[i] = cmd[i + 4];
-            }
-            // memcpy(&v[0], &vbuffer[0], sizeof(v));
-            v[0] = *((float*)&vbuffer[0]);
-            v[1] = *((float*)&vbuffer[4]);
-            v[2] = *((float*)&vbuffer[8]);
-            v[3] = *((float*)&vbuffer[12]);
+                char vbuffer[16];
+                int p;
+                for(i = 0; i < 16; i++){
+                    
+                    vbuffer[i] = cmd[i + 4];
+                }
+                // memcpy(&v[0], &vbuffer[0], sizeof(v));
+                v[0] = *((float*)&vbuffer[0]);
+                v[1] = *((float*)&vbuffer[4]);
+                v[2] = *((float*)&vbuffer[8]);
+                v[3] = *((float*)&vbuffer[12]);
 
-            this->ardrone.pcmdCommand(true, v);
-            
-            Serial.println("Controle");
+                this->ardrone.pcmdCommand(true, v);
+                
+                Serial.println("Controle");
             }
         }
 
