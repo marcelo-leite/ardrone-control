3f8767ef176de37f7d04bf223aec9023f2b41445
marcelo-leite <leite_marcelo@outlook.com>
2021-09-11 10:44:57 -0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,27 @@ void loop() {
   while (WiFi.status() == WL_CONNECTED) {
       // rfcontrol.sendData();
       // rfcontrol.ardrone.takeoff();
-      rfcontrol.captureData();
-      // rfcontrol.secureHealth();
-      // if(rfcontrol.data.fligth_data.adrone_state & 1){
-      //   // rfcontrol.control_wz(60.0);
-      //   Serial.print("Voando");
+      // delay(10000);
+      // rfcontrol.ardrone.takeoff();
+      while (WiFi.status() == WL_CONNECTED) {
+        rfcontrol.captureData();
+        // rfcontrol.secureHealth();
+        // if(rfcontrol.data.fligth_data.adrone_state & 1){
+        //   // rfcontrol.control_wz(60.0);
+        //   Serial.print("Voando");
 
-      // }else{
-      //   Serial.print("Aterrissado");
-      // }
-      
-      // if(Serial.available()){
-      //   rfcontrol.receiveData();
-      // }
-      // rfcontrol.control_wz(120.0);
-      
-      // Serial.println(rfcontrol.ardrone.getArdata().fligth_data.psi);
-      delay(100);
+        // }else{
+        //   Serial.print("Aterrissado");
+        // }
+        
+        // if(Serial.available()){
+        //   rfcontrol.receiveData();
+        // }
+        // rfcontrol.control_wz(120.0);
+        
+        // Serial.println(rfcontrol.ardrone.getArdata().fligth_data.psi);
+        delay(200);
+      }
   }
  
   Serial.println("disconnected from AR, attempting to reconnect");
